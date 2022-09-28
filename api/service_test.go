4ff4b24a5ac13a63b59c39b71e6d13a4d027141a
Kewin Rémy <kewin.remy@juliusbaer.com>
2022-09-28 21:14:24 +0200

    adding tests

--- api/service_test.go
+++ api/service_test.go
@@ -1,39 +1,88 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-var testItems = []Item{
-	{
-		Id:            1,
-		Name:          "panosit",
-		ReservationId: 387,
-	},
-	{
-		Id:            1,
-		Name:          "tisonap",
-		ReservationId: 783,
-	},
-}
+	"github.com/gin-gonic/gin"
+)
 
 type repoFake struct {
+	items []Item
+	err   error
 }
 
-func (r *repoFake) ListItems() ([]Item, error) {
-	return testItems, nil
+func (r repoFake) ListItems() ([]Item, error) {
+	return r.items, nil
 }
 
-func (r *repoFake) CreateItem(Item) error {
-	return nil
+func (r repoFake) CreateItem(Item) error {
+	return r.err
 }
 
 func TestListItems(t *testing.T) {
+	tests := []struct {
+		name string
+		want []Item
+	}{
+		{
+			name: "List with items",
+			want: []Item{
+				{
+					Id:            1,
+					Name:          "panosit",
+					ReservationId: 387,
+				},
+				{
+					Id:            1,
+					Name:          "tisonap",
+					ReservationId: 783,
+				},
+			},
+		},
+		{
+			name: "Empty List",
+			want: []Item{},
+		},
+	}
+
 	// given
-	repo := new(repoFake)
-	service = NewService(repo)
+	w := httptest.NewRecorder()
+	gc, _ := gin.CreateTestContext(w)
+	repoFake := new(repoFake)
+	service = new(Service)
+	service.repo = repoFake
+	var got []Item
 
 	// when
-	service.repo.ListItems()
+	for _, tt := range tests {
+		repoFake.items = tt.want
 
-	// then
+		service.GetItems(gc)
 
+		// then
+		if w.Code != http.StatusOK {
+			log.Printf("GetItems failed, expected %v got %v\n", http.StatusOK, w.Code)
+			t.Error()
+			return
+		}
+
+		body, _ := ioutil.ReadAll(w.Body)
+		err := json.Unmarshal(body, &got)
+		if err != nil {
+			log.Printf("Test failed due bad decoding\n")
+			t.Error()
+			return
+		}
+
+		if len(got) != len(tt.want) {
+			log.Printf("Test failed due bad length, expected %v got %v\n", len(tt.want), len(got))
+			t.Error()
+			return
+		}
+	}
 }
